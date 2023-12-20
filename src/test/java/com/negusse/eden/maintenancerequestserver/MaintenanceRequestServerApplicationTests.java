ea58811fa504e1698869c703482a694430e2fb18
Student 35 <student35@Codes-MacBook-Air-59.local>
2023-12-20 15:18:23 -0500

    creating repo

---
+++ src/test/java/com/negusse/eden/maintenancerequestserver/MaintenanceRequestServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.negusse.eden.maintenancerequestserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaintenanceRequestServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
